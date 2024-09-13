9fbf089bc0501872d608af9babc824a46b31d26e
aylonmelos1 <aylon.melo@outlook.com>
2024-09-13 13:56:46 -0300

    new: exibe alguma coisa na rota /

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,15 @@ app.use(express.json());
 // Adicionar o uso do roteador
 app.use('/api', api);
 
+app.get('/', (req, res) => {
+    res.json({
+        "status": 200,
+        "message": "RandomSellers, está rodando!",
+        "version": "1.0.0",
+        "endpoint": "http://localhost:3387/randomSellers",
+        "method": "GET"});
+});
+
 httpServer.listen(3387, () => {
     console.log(`Aplicação rodando na porta 3387`);
 });
